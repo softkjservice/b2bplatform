da8a138afa66a962266b3e4d23cf2dcf1586bed3
softkj <kj@ploter.eu>
2023-04-27 21:15:39 +0200

    added foreign key to poducts table

--- database/migrations/2023_04_14_113526_create_products_table_poUsunieciuProducts.php
+++ database/migrations/2023_04_14_113526_create_products_table_poUsunieciuProducts.php
@@ -24,7 +24,8 @@ return new class extends Migration
             $table->string('currency',5);
             $table->tinyInteger('vatrate');
             $table->text('description');
-            $table->integer('category_id');
+            $table->integer('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->string('priority',10);
             $table->timestamps();
         });

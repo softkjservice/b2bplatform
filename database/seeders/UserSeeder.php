6334e9b6d62864ba00965830b240d7140f890565
softkj <kj@ploter.eu>
2023-06-30 18:34:02 +0200

    Correction in UserSeeder - added email_verifield_at

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,16 +20,19 @@ class UserSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'kj@ploter.eu',
             'role' => 'admin',
+            'email_verified_at' => '2023-06-30 16:24:16',
             'password' => '$2y$10$3HfyZ2yd/mrxoa1kjr02DeFREPa9945BEnv9oO0nqd3o8NrQ9L0PS'],
             [
                 'name' => 'User',
                 'email' => 'user@ploter.eu',
                 'role' => 'user',
+                'email_verified_at' => '2023-06-30 16:24:16',
                 'password' => '$2y$10$3HfyZ2yd/mrxoa1kjr02DeFREPa9945BEnv9oO0nqd3o8NrQ9L0PS'],
             [
                 'name' => 'Gość',
                 'email' => 'gosc@ploter.eu',
                 'role' => 'user',
+                'email_verified_at' => '2023-06-30 16:24:16',
                 'password' => '$2y$10$3HfyZ2yd/mrxoa1kjr02DeFREPa9945BEnv9oO0nqd3o8NrQ9L0PS']
         ]);
     }
